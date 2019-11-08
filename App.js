0b2217e4d592c5324e43cc1da11af9d10a26ecc3
Stiv Márquez <stivmarquez.ud@gmail.com>
2019-11-08 15:06:19 -0500

    Implementación Parcial Sitio

--- App.js
+++ App.js
@@ -25,18 +25,52 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import {WebView} from 'react-native-webview';
-
 import StaticServer from 'react-native-static-server';
-
-let server = new StaticServer(8080);
+import RNFS from 'react-native-fs';
 
 export default class App extends Component {
+  //
+  //
+  // serverStart() {
+  //   this.server.start();
+  // }
+  path = RNFS.DocumentDirectoryPath + '/www';
+  server = new StaticServer(8080, this.path);
+  state = {
+    url: null,
+  };
+  file = 'index.html';
+
+  moveAndroidFiles() {
+    RNFS.mkdir(RNFS.DocumentDirectoryPath + '/www');
+    this.files = ['www/index.html'];
+    // this.files.forEach(async file => {
+    RNFS.copyFileAssets(
+      'www/index.html',
+      RNFS.DocumentDirectoryPath + '/' + 'www/index.html',
+    );
+    // });
+  }
   render() {
+    // this.serverStart();
+    // isRunning = this.server.isRunning();
+    this.moveAndroidFiles();
+
+    this.server.start().then(url => {
+      this.setState({url});
+    });
+
+    // this.server.start();
+    //
+    // console.log('stiv');
+    // console.log(this.path);
+    // console.log(this.server);
+    console.log(`${this.state.url}/${this.file}`);
     return (
       <SafeAreaView>
         <View style={{height: '100%', width: '100%'}}>
           <WebView
-            source={{uri: 'https://infinite.red'}}
+            source={{uri: `${this.state.url}/${this.file}`}}
             style={{marginTop: 20}}
           />
         </View>
